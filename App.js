0a15a4672741ca5d67c85304804c1d7c74a7eebd
Crispim14 <132013854+Crispim14@users.noreply.github.com>
2024-10-02 20:54:58 -0300

    sidebar

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 // App.js
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import AddScheduleScreen from './screens/AddScheduleScreen';
+import TestScreen from './screens/TestScreen';
 import { createTable } from './database/scheduleDB';
 
 const Stack = createStackNavigator();
-
+const Drawer = createDrawerNavigator();
 export default function App() {
     useEffect(() => {
         createTable(); // Cria a tabela ao iniciar o aplicativo
@@ -15,24 +17,52 @@ export default function App() {
 
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Agendamentos', headerStyle: {
-      backgroundColor: '#1A2833'
-   
-    },
-    headerTitleStyle :{
-        color: '#E3E3E3', 
-    }
-    
-    }} />
-                <Stack.Screen name="AddSchedule" component={AddScheduleScreen} options={{ title: 'Novo Agendamento' , headerStyle: {
-      backgroundColor: '#1A2833'
-   
-    },
-    headerTitleStyle :{
-        color: '#E3E3E3', 
-    }}} />
-            </Stack.Navigator>
+            <Drawer.Navigator screenOptions={{
+                drawerStyle: {
+                    backgroundColor: '#1A2833',
+                },
+                drawerLabelStyle: {
+                    color: '#E3E3E3',
+                },
+                headerTintColor: '#E3E3E3',
+                
+            }} initialRouteName="Home">
+                <Drawer.Screen name="Home" component={HomeScreen} options={{
+                    title: 'Agendamentos', headerStyle: {
+                        backgroundColor: '#1A2833'
+
+                    },
+                    headerTitleStyle: {
+                        color: '#E3E3E3',
+                    }
+
+                }} />
+                <Drawer.Screen name="AddSchedule" component={AddScheduleScreen}
+                    options={{
+                        title: 'Novo Agendamento',
+                        headerStyle: { backgroundColor: '#1A2833' },
+                        headerTitleStyle: { color: '#E3E3E3', }
+                    }} />
+            </Drawer.Navigator>
+            {/* <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} options={{
+                    title: 'Agendamentos', headerStyle: {
+                        backgroundColor: '#1A2833'
+
+                    },
+                    headerTitleStyle: {
+                        color: '#E3E3E3',
+                    }
+
+                }} />
+                <Stack.Screen name="AddSchedule" component={AddScheduleScreen} options={{
+                    title: 'Novo Agendamento', headerStyle: {
+                        backgroundColor: '#1A2833'
+                    }, headerTitleStyle: { color: '#E3E3E3', }
+                }}
+                />
+            </Stack.Navigator> */}
         </NavigationContainer>
+
     );
 }
